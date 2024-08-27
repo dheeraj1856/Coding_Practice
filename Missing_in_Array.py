43f6b329d3f35a569b2f12c3ca3287e3645c9d29
Dheeraj Kumar <144531592+dheeraj1856@users.noreply.github.com>
2024-08-27 03:43:20 +0300

    Update Missing_in_Array.py

--- Missing_in_Array.py
+++ Missing_in_Array.py
@@ -1,3 +1,6 @@
+# Given an array arr of size n−1 that contains distinct integers in the range of 1 to n (inclusive), find the missing element. 
+# The array is a permutation of size n with one element missing. Return the missing element.
+
 class Solution:
     def missingNumber(self,array,n):
         # code here
