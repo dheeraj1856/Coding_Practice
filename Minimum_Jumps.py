b8f0ed8dc17df4fb47ddcb471f9f0cafcc95c197
Dheeraj Kumar <144531592+dheeraj1856@users.noreply.github.com>
2024-08-27 03:35:34 +0300

    Create Minimum_Jumps.py

---
+++ Minimum_Jumps.py
@@ -0,0 +1,32 @@
+# Given an array arr[] of size n of non-negative integers. Each array element represents the maximum length of the jumps that can be made forward from that element. This means if arr[i] = x, then we can jump any distance y such that y ≤ x.
+# Find the minimum number of jumps to reach the end of the array starting from the first element. If an element is 0, then you cannot move through that element.
+# Note:  Return -1 if you can't reach the end of the array.
+
+class Solution:
+    def minJumps(self, arr, n):
+        #code here
+        if n == 1:
+            return 0
+        if arr[0] == 0:
+            return -1
+    
+        jumps = 1
+        maxReach = arr[0]
+        steps = arr[0]
+        
+        for i in range(1, n):
+            if i == n - 1:
+                return jumps
+            
+            maxReach = max(maxReach, i + arr[i])
+            steps -= 1
+            
+            if steps == 0:
+                jumps += 1
+                if i >= maxReach:
+                    return -1
+                steps = maxReach - i
+    
+        return -1
+                
+               
