9569f6b046f6a66b27d01ba91cb1d224a449b592
Dheeraj Kumar <144531592+dheeraj1856@users.noreply.github.com>
2024-08-27 03:45:13 +0300

    Update Indexes_of_Subarray_Sum.py

--- Indexes_of_Subarray_Sum.py
+++ Indexes_of_Subarray_Sum.py
@@ -1,3 +1,9 @@
+# Given an unsorted array arr of size n that contains only non negative integers, find a sub-array (continuous elements) that has sum equal to s. 
+# You mainly need to return the left and right indexes(1-based indexing) of that subarray.
+# In case of multiple subarrays, return the subarray indexes which come first on moving from left to right. 
+# If no such subarray exists return an array consisting of element -1.
+
+
 #Function to find a continuous sub-array which adds up to a given number.
 class Solution:
     def subArraySum(self,arr, n, s): 
